64348654968d9eec067e0a83b8d2db485abce00e
Paulo Muniz <94541244+paullomuniz@users.noreply.github.com>
2021-12-20 08:32:00 -0300

    Add files via upload

---
+++ Picture Resize/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Picture_Resize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtPathSelectectImage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnSaveAss = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(417, 41);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Select Image";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtPathSelectectImage
+            // 
+            this.txtPathSelectectImage.Location = new System.Drawing.Point(65, 43);
+            this.txtPathSelectectImage.Name = "txtPathSelectectImage";
+            this.txtPathSelectectImage.ReadOnly = true;
+            this.txtPathSelectectImage.Size = new System.Drawing.Size(346, 20);
+            this.txtPathSelectectImage.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "New Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "New Height";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(249, 128);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(249, 161);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // btnSaveAss
+            // 
+            this.btnSaveAss.Location = new System.Drawing.Point(249, 234);
+            this.btnSaveAss.Name = "btnSaveAss";
+            this.btnSaveAss.Size = new System.Drawing.Size(100, 23);
+            this.btnSaveAss.TabIndex = 6;
+            this.btnSaveAss.Text = "Resize My Pic";
+            this.btnSaveAss.UseVisualStyleBackColor = true;
+            this.btnSaveAss.Click += new System.EventHandler(this.btnSaveAss_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 359);
+            this.Controls.Add(this.btnSaveAss);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPathSelectectImage);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtPathSelectectImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnSaveAss;
+    }
+}
+
---
+++ Picture Resize/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace Picture_Resize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            string lokasi = file.InitialDirectory;
+            lokasi = @"C:\Users\ardha\Desktop";
+            file.Title = "Open Image File";
+            file.Filter = "Image Files(*.jpg,*.png,*.tiff,*.bmp,*.gif)|*.jpg;*.png;*.tiff;*.bmp;*.gif";
+            file.FilterIndex = 2;
+            file.RestoreDirectory = true;
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                txtPathSelectectImage.Text = file.FileName;
+            }
+        }
+
+        private void btnSaveAss_Click(object sender, EventArgs e)
+        {
+            if(txtPathSelectectImage.Text == "")
+            {
+                MessageBox.Show("Choose a pic for resize.");
+            }
+            if (txtHeight.Text == "" || txtWidth.Text == "")
+            {
+                MessageBox.Show("Choose a new sizes for the pic.");
+            }
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = @"C:\Users\ardha\Desktop";
+            saveFile.Filter = "Image  Files(*.jpg, *.png, *.tiff, *.bmp, *.gif) | *.jpg; *.png; *.tiff; *.bmp; *.gif";
+            saveFile.Title = "Save an image";
+            saveFile.AddExtension = true;
+            saveFile.DefaultExt = "bmp";
+            saveFile.FilterIndex = 2;
+            saveFile.RestoreDirectory = true;
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(txtPathSelectectImage.Text);
+                int new_height = Convert.ToInt32(txtHeight.Text);
+                int new_width = Convert.ToInt32(txtWidth.Text);
+
+                Bitmap new_image = new Bitmap(new_width, new_height);
+                Graphics g = Graphics.FromImage((Image)new_image);
+                g.InterpolationMode = InterpolationMode.High;
+                g.DrawImage(image, 0, 0, new_width, new_height);
+
+                new_image.Save(saveFile.FileName);
+
+
+            }
+
+            MessageBox.Show("Save successfully");
+
+
+        }
+    }
+}
